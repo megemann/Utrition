e4f6768a83280767797af614544ce93a8cdebdc7
Austin Fairbanks <89821947+megemann@users.noreply.github.com>
2024-01-05 13:55:21 -0500

    Add DiningHallPage and Backend Blueprint

---
+++ src/Components/DiningHallPage/DiningHallPage.jsx
@@ -0,0 +1,15 @@
+import TopAppBar from "../TopAppBar/TopAppBar";
+import s from "./style.module.css"
+
+
+export default function DiningHallPage({hallName}) {
+    return (
+        <>
+            <TopAppBar/>
+            <div className={s.background}>
+
+            </div>
+        </>
+
+    );
+}
--- src/Components/NavDrawer/NavDrawer.tsx
+++ src/Components/NavDrawer/NavDrawer.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Link, Routes, Route } from "react-router-dom"; //for routing to new webpages
 import HomePage from "../HomePage/HomePage";
+import DiningHallPage from "../DiningHallPage/DiningHallPage";
 
 
 export default function NavDrawer(){
@@ -9,10 +10,10 @@ export default function NavDrawer(){
             <Routes>
                 <Route path={"/"} element={<HomePage/>}/>
                 <Route path={"/nav"} element={<HomePage/>}/>
-                <Route path={"/Worcester"} element={<HomePage/>}/>
-                <Route path={"/Franklin"} element={<HomePage/>}/>
-                <Route path={"/Hampshire"} element={<HomePage/>}/>
-                <Route path={"/Berkshire"} element={<HomePage/>}/>
+                <Route path={"/Worcester"} element={<DiningHallPage hallName={"Worcester"}/>}/>
+                <Route path={"/Franklin"} element={<DiningHallPage hallName={"Franklin"}/>}/>
+                <Route path={"/Hampshire"} element={<DiningHallPage hallName={"Hampshire"}/>}/>
+                <Route path={"/Berkshire"} element={<DiningHallPage hallName={"Berkshire"}/>}/>
             </Routes>
         </div>
         </BrowserRouter>
